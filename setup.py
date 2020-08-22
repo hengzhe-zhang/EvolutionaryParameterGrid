fc0fe5966f8086cebc74148466c3656d780e28b9
zhenlingcn <zhanghengzhe@corp.netease.com>
2020-08-22 19:56:18 +0800

    1. update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='EvolutionaryParameterGrid',
-    version='0.0.2',
+    version='0.0.3',
     author='ZhangHengzhe',
     author_email='',
     description='Use evolutionary algorithms instead of GridParameter in scikit-learn.',
